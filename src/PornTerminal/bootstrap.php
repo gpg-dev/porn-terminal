73e77286f5400eb0b19ecda56bcfa5096e2ecd53
redaxmedia <info@redaxmedia.com>
2017-09-29 19:40:21 +0200

    Move everything to the bootstrap

--- src/PornTerminal/bootstrap.php
+++ src/PornTerminal/bootstrap.php
@@ -6,3 +6,23 @@ error_reporting(E_ERROR | E_PARSE);
 /* include */
 
 include_once('.' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+/* api */
+
+$api = new Api();
+$api->init();
+
+/* wording */
+
+$wording = new Wording();
+$wording->init();
+
+/* command */
+
+$command = new Command($api, $wording);
+$command->init();
+
+/* core */
+
+$core = new Core($api, $wording);
+echo $core->run($command);
